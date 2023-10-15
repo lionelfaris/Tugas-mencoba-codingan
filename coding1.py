9b70f7808e397b1284f9c7e3670241c76b141a70
lionelfaris <147967585+lionelfaris@users.noreply.github.com>
2023-10-15 13:41:54 +0700

    Add files via upload

---
+++ coding1.py
@@ -0,0 +1,9 @@
+def luas_segitiga (a,t) :
+    return a * t /2
+print (luas_segitiga (2,4))
+
+def untung (modal, jual) :
+    return (modal - jual) * -1
+print ("keuntungan anda adalah", untung (int (input ("Masukkan modal : ")), (int (input ("Masukkan harga jual : ")))))
+
+print "hi"
